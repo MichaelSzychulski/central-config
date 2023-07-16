7d3edf5bb49521a2b54a2f07430bf74083c0e6f7
MichaelSzychulski <michael.szychulski@gmail.com>
2023-07-16 18:50:21 +0200

    Added project files

---
+++ example/client1/src/test/kotlin/com/example/centralConfigClient/CentralConfigClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.centralConfigClient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CentralConfigClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
